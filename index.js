109084a6ac9537286eea3a1f3b769a0f071596bb
Lucije <55711219+Lucije@users.noreply.github.com>
2020-05-18 18:57:10 +0200

    Hotovo

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 'use strict';
 
 console.log('funguju!');
+
+const tasks = ['Vyprat ponožky', 'Naučit se funkce vyššího řádu', 'Nakoupit na víkend'];
+
+const updateTasks = () => {
+    const todoElm = document.querySelector('.todo__tasks');
+    todoElm.innerHTML = ``;
+    for (let i = 0; i < tasks.length; i++) {
+        todoElm.innerHTML += `<div class="task">${tasks[i]}</div>`
+    }
+}
+
+
+const addTask = () => {
+    const newTaskElm = document.querySelector('#new-task');
+
+    if (newTaskElm.value === '') {
+        return false
+    }
+    else {
+        tasks.push(newTaskElm.value);
+        updateTasks();
+        newTaskElm.value = ''
+    }
+}
+const btnElm = document.querySelector('.btn-add');
+btnElm.addEventListener('click', addTask);
+
+document.querySelector('#new-task').addEventListener('click', function (e) {
+    if (e.key === 'Enter') {
+        addTask()
+    }
+
+});
+
+updateTasks()
+
+
